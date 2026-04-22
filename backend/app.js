b38592af0096c383de8a197535f90a56a48826db
Prashant Kumar <prashantkumar4216@gmail.com>
2026-04-22 22:09:42 +0530

    Make UserRoute

--- backend/app.js
+++ backend/app.js
@@ -19,7 +19,8 @@ const io = new Server(server, {
 
 // Middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "40kb" }));
+app.use(express.urlencoded({ limit: "40kb", extended: true }))
 
 // Route
 app.get("/", (req, res) => {
---
+++ backend/controllers/users.routes.js
@@ -0,0 +1,9 @@
+const Router = require(express());
+
+router = Router();
+
+router.route("/login")
+router.route("/register")
+router.route("/Login")
+
+export default router
